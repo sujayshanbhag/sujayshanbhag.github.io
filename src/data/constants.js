99364db6b887c5f6b9c84317aa6d67e6183a3f49
SujayShanbhag <SujayShanbhag@users.noreply.github.com>
2025-09-04 22:15:17 +0530

    updated resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,8 +8,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/SujayShanbhag",
-  resume:
-    "https://drive.google.com/file/d/1qWrLdkwUm6kuVW3enZIfWtdeyA-cpe9L/view?usp=drive_link",
+  resume: "https://drive.google.com/file/d/1lI5OESWw2ThqCMMm3csFCdC-woi8_bIo/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/sujay-shanbhag-94b936249",
   twitter: "https://twitter.com/sujay_shanbhag",
   insta: "https://www.instagram.com/sujay_shanbhag/",
